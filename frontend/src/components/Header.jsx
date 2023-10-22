7ec5517ee84e1f127cb59f961d44f2e8f4554300
Ketan Kudikyal <krotusppp@gmail.com>
2023-10-22 20:52:29 +0530

    fix: added safe wallet

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import * as React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LoginVerifier from "./LoginModal";
 
 export default function Navigation() {
@@ -37,12 +37,12 @@ export default function Navigation() {
           variant="dense"
           style={{
             display: "flex",
-            justifyContent: "end",
+            justifyContent: "space-between",
             width: "100%",
             flexDirection: "row",
           }}
         >
-          {/* <Typography variant="h6" color="inherit" component="div">
+          <Typography variant="h6" color="inherit" component="div">
             <Link
               to={"/"}
               style={{
@@ -50,7 +50,7 @@ export default function Navigation() {
                 alignItems: "center",
               }}
             >
-              Get a Loan Today - Powered by{" "}
+              Abstrakt ID Powered by{" "}
               <img
                 src="https://assets-global.website-files.com/637359c81e22b715cec245ad/63dc31f8817a4a509d7635a7_Logo.svg"
                 style={{
@@ -60,27 +60,31 @@ export default function Navigation() {
                 }}
                 alt="polygon"
               />{" "}
+              &nbsp; and
+              <h1 className="ml-2">Safe Wallet</h1>
             </Link>
-          </Typography> */}
-          <Button
-            variant="contained"
-            style={{
-              backgroundColor: "#8d2cab",
-              fontWeight: "normal",
-              color: "#fff",
-              marginRight: "10px",
-            }}
-            onClick={() => {
-              if (localStorage.getItem("user_did")) {
-                navigate("/claim");
-              } else {
-                setOpen(true);
-              }
-            }}
-          >
-            Create a new claim
-          </Button>
-          <ConnectButton />
+          </Typography>
+          <div className="flex items-center space-x-2 justify-end">
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "#8d2cab",
+                fontWeight: "normal",
+                color: "#fff",
+                marginRight: "10px",
+              }}
+              onClick={() => {
+                if (localStorage.getItem("user_did")) {
+                  navigate("/claim");
+                } else {
+                  setOpen(true);
+                }
+              }}
+            >
+              Create a new claim
+            </Button>
+            <ConnectButton />
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
