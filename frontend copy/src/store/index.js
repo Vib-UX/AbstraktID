160955a9b2e418d715809c4ca47583266fc6a70e
Ketan Kudikyal <krotusppp@gmail.com>
2023-10-22 20:38:15 +0530

    fix: final changes

--- frontend copy/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useGlobalStore = create((set) => ({
-  user_did: null,
-  setUserDid: () => set((state) => ({ user_did: state.user_did })),
-}));
-
-export default useGlobalStore;
